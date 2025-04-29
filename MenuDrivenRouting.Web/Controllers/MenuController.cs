81d2982f0f61b568acded3dc6383b1fd4f28cbc8
leonard <vinyasleonard.lobo@broadridge.com>
2025-04-29 12:43:43 +0000

    Menu

---
+++ MenuDrivenRouting.Web/Controllers/MenuController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MenuDrivenRouting.Web.Controllers;
+public class MenuController : Controller
+{
+    public IActionResult Home()
+    {
+        return View();
+    }
+
+    public IActionResult Customers()
+    {
+        return View();
+    }
+
+    public IActionResult Products()
+    {
+        return View();
+    }
+
+    public IActionResult Orders()
+    {
+        return View();
+    }
+}
